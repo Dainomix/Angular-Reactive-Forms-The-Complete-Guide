6955de8f642d0fa9c902615b24c61428efae8836
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-30 22:44:51 +0900

    Reactive: Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   genders = ['male', 'female'];
+
+  /**
+   * In a TD approach, we already imported ngFrom from it (@angular/forms)
+   * now ngForm was this automatically created wrapper.
+   * we could say but it was wrapping a FormGroup in the end, because in Angular, a form in the end is just a group of controls.
+   * and this is what a form group holds.
+   * 
+   * Therefore, the overall form also is just a form group.
+   * This gives us a property with which we can work which will hold our form in the ed.
+   */
+  signupForm: FormGroup;
+
 }

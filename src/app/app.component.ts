023e824f9b688af25ad86de1b3d3d9009721a919
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-30 22:57:48 +0900

    Reactive: Creating a Form in Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
 
   /**
-   * In a TD approach, we already imported ngFrom from it (@angular/forms)
+   * In a TD approach, we already imported ngForm from it (@angular/forms)
    * now ngForm was this automatically created wrapper.
    * we could say but it was wrapping a FormGroup in the end, because in Angular, a form in the end is just a group of controls.
    * and this is what a form group holds.
@@ -19,5 +19,16 @@ export class AppComponent {
    * This gives us a property with which we can work which will hold our form in the ed.
    */
   signupForm: FormGroup;
+
+  ngOnInit() {
+    // FormControl(initial state of this control
+    //            , single validator or an array of validator that we want to apply to this control
+    //            , potential asynchronous validators)
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+    });
+  }
 
 }
